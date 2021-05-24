5e84b3c58cd522e79a4c4beceb87ef34631835ac
ectodrop <40843847+ectodrop@users.noreply.github.com>
2021-05-24 02:27:56 -0400

    .

---
+++ GameOver.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameOver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameOver extends World
+{
+
+    /**
+     * Constructor for objects of class GameOver.
+     * 
+     */
+    private static int highscore = 0;
+    public GameOver(int score)
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(18, 22, 16, false);
+        if(highscore < score) highscore = score;
+        getBackground().setColor(Color.BLACK);
+        getBackground().fill();
+        addObject(new Text("High score: " + highscore, Color.WHITE), 10, 20);
+        addObject(new Text("Score: " + score, Color.WHITE), 11, 18);
+        
+        setBackground("game_over.png");
+        if(Grid.music.isPlaying()){
+            Grid.music.stop();
+        }
+    }
+    
+    public void act(){
+        if(Greenfoot.isKeyDown("Enter")){
+            Greenfoot.setWorld(new Grid());
+        }
+    }
+}
---
+++ Start.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Start here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Start extends World
+{
+
+    /**
+     * Constructor for objects of class Start.
+     * 
+     */
+    public Start()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(18, 22, 16, false);
+        setBackground("startbackground.png");
+        for(int i = 0; i < 18; i++){
+            for(int j = 15; j < 22; j++){
+                Tile t = new Tile();
+                t.setImage(Greenfoot.getRandomNumber(7) + ".png");
+                addObject(t, i , j);
+            }
+        }
+        if(Grid.music.isPlaying()){
+            Grid.music.stop();
+        }
+    }
+    public void act(){
+        if(Greenfoot.isKeyDown("Enter")){
+            Greenfoot.setWorld(new Grid());
+        }
+    }
+}
